7bcd8c12fcb60598133662efff9b0bf80d16d21e
Pranjal Agarwal <pranjalagarwal6@gmail.com>
2022-12-18 21:30:41 +0530

    try 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,9 +2,15 @@ package com.driver;
 
 public class Main {
     public static class Product{
-        public int product(int x, int y) {}
-        public int product(int x, int y, int z) {}
-        public double product(double x, double y) {}
+        public int product(int x, int y) {
+            return x+y;
+        }
+        public int product(int x, int y, int z) {
+            return x+y+z;
+        }
+        public double product(double x, double y) {
+            return x+y;
+        }
     }
 
     public static void main(String args[]){
