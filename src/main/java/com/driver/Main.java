db89155698d8b74d021a24980d113f5208ef06c7
Pranjal Agarwal <pranjalagarwal6@gmail.com>
2022-12-19 21:39:46 +0530

    okkk

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,21 +3,22 @@ package com.driver;
 public class Main {
     public static class Product{
         public int product(int x, int y) {
-            return x+y;
+            return x*y;
         }
         public int product(int x, int y, int z) {
-            return x+y+z;
+            return x*y*z;
         }
         public double product(double x, double y) {
-            return x+y;
+            return x*y;
         }
     }
+    public static void main(String []args){
+        Product p=new Product();
+        int a=p.product(2,3);
+        int b=p.product(3,3,10);
+        double c=p.product(2.3,30.1);
 
-    public static void main(String args[]){
-        Product p = new Product();
-        int a = p.product(10,20);
-        int b = p.product(10,20,30);
-        double c = p.product(10.0,20.0);
         System.out.println(a+" "+b+" "+c);
     }
+
 }
